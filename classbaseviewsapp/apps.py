cb69fd6ab99215969e287b236202d4110a06cc7a
Subhajyoti Dash <dashjitu3@gmail.com>
2019-04-03 16:56:34 +0530

    Initial commit

---
+++ classbaseviewsapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ClassbaseviewsappConfig(AppConfig):
+    name = 'classbaseviewsapp'
---
+++ classbaseviewsapp/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.views.generic import View
+from django.http import HttpResponse
+from django.shortcuts import render
+# Create your views here.
+class GetInput(View):
+    def get(self,request):
+        return render(request,'getinput.html')
+class PostInput(View):
+    def get(self,request):
+        return render(request,'postinput.html')
+class Add(View):
+    def get(self,request):
+        try:
+            a = request.GET['t1']
+            x = int(a)
+            b = request.GET['t2']
+            y = int(b)
+            z = x + y
+            return HttpResponse("<html><body bgcolor=cyan><h1>sum is:" + str(z) + "</h1></body></html>")
+        except(ValueError):
+            return HttpResponse("Invalid input")
+    def post(self,request):
+        try:
+            a = request.POST['t1']
+            x = int(a)
+            b = request.POST['t2']
+            y = int(b)
+            z = x + y
+            return HttpResponse("<html><body bgcolor=cyan><h1>sum is:" + str(z) + "</h1></body></html>")
+        except(ValueError):
+            return HttpResponse("Invalid input")
+
+
